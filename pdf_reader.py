d0b3c71939c54b9300f8455773335da46cb29ba3
Edgar Jacquemoud <cookiesarenightmares@gmail.com>
2024-05-24 00:29:03 +0200

    parsed ibm + c kinds from pdf

---
+++ pdf_reader.py
@@ -0,0 +1,74 @@
+from PyPDF2 import PdfReader
+
+import tabula
+import re
+reader = PdfReader("IBM_ILE.pdf")
+table_reader = tabula.read_pdf("IBM_ILE.pdf", pages=[i for i in range(32 , 75)])
+
+c_functions = []
+
+for frame in table_reader:
+    for row in frame.to_dict('records'):
+        print(row)
+        func = str(row["Function"]).replace('\r', '')
+        desc = str(row["Description"]).replace('\r', ' ')
+
+        if func != 'nan':
+            if len(desc) > 100:
+                desc = f'{desc[:50]}...'
+            tuple = (func, desc)
+            
+            c_functions.append(tuple)
+
+print(c_functions)
+#add token kinds
+with open("./utils/lex/TokenKinds.py", 'r', encoding="utf-8") as file:
+    content = file.read()
+    section = "### IBM RELATED ###"
+
+    kinds = section + '\n' + "\n".join([f'\tTOKEN_IBM_{func[0].strip('()').upper()} = "{func[1]}"' for func in c_functions])
+    #kinds = section + '\n' + "\n".join([f'\tTOKEN_C_{func[0].strip('()').upper()} = "{func[1]}"' for func in c_functions])
+    content = content.replace(section, kinds )
+    with open("./utils/lex/parsedKinds.txt", 'w', encoding='utf-8') as out:
+        out.write(content)
+    
+
+# add tables
+with open("./utils/lex/TokenTables.py", 'r+', encoding="utf-8") as file:
+    content = file.read()
+    table_name = "IBM_KEYWORDS_TOKEN"
+    template = """
+{tableName} = {{
+    {funcName}: 
+}}
+"""
+
+# number_of_pages = len(reader.pages)
+
+# TABLE_HEADER = "Function Header File Page Description"
+# PAGE_FOOTERS = ["IBM i: ILE C/C++ Runtime Library Functions", "Library Functions"]
+
+# functions = {}
+# for i in range(32,75):
+#     text = reader.pages[i].extract_text()
+#     lines = text.splitlines()
+#     desc = ""
+#     currentPage = i
+#     print(text.strip('\r'))
+#     for j, line in enumerate(lines):
+#         # false_flag =  "IBM i: ILE C/C++ Runtime Library Functions" in line and lines[j+1] == TABLE_HEADER
+        
+#         if line == "Function Header File Page Description":
+#             desc = lines[j-1] 
+#             for footer in PAGE_FOOTERS:
+#                 if footer in desc:
+#                     desc = previous
+                   
+        
+#         if (regex:= re.search(r'^\b\w+\(\)', line)) != None and len(desc) > 0:
+#             functions.setdefault(desc.strip(), set()).add(regex.group(0).strip("()"))
+#             previous = desc
+    
+# print(functions)
+
+# print(functions['Handling Argument Lists'])
